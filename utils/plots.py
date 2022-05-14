c4fabfef4112e3859ded147095b7577d7b8a40f0
Mehmet <sumer682000@gmail.com>
2022-05-14 14:39:00 +0300

    font size

--- utils/plots.py
+++ utils/plots.py
@@ -75,7 +75,7 @@ class Annotator:
             self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
             self.draw = ImageDraw.Draw(self.im)
             self.font = check_pil_font(font='Arial.Unicode.ttf' if non_ascii else font,
-                                       size=font_size or max(round(sum(self.im.size) / 2 * 0.035), 12))
+                                       size=font_size or max(round(sum(self.im.size) / 2 * 0.016), 7))
         else:  # use cv2
             self.im = im
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
