8f86f763c4392a0fd69e38648d5eb19a85527ad0
Mehmet <sumer682000@gmail.com>
2022-05-14 15:08:13 +0300

    font size 4

--- utils/plots.py
+++ utils/plots.py
@@ -49,7 +49,7 @@ class Colors:
 colors = Colors()  # create instance for 'from utils.plots import colors'
 
 
-def check_pil_font(font=FONT, size=5):
+def check_pil_font(font=FONT, size=2):
     # Return a PIL TrueType Font, downloading to CONFIG_DIR if necessary
     font = Path(font)
     font = font if font.exists() else (CONFIG_DIR / font.name)
@@ -75,7 +75,7 @@ class Annotator:
             self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
             self.draw = ImageDraw.Draw(self.im)
             self.font = check_pil_font(font='Arial.Unicode.ttf' if non_ascii else font,
-                                       size=max(round(sum(self.im.size) / 2 * 0.016), 7))
+                                       size=2)
         else:  # use cv2
             self.im = im
         self.lw = line_width or max(round(sum(im.shape) / 2 * 0.003), 2)  # line width
@@ -106,7 +106,7 @@ class Annotator:
                 cv2.putText(self.im,
                             label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2),
                             0,
-                            self.lw / 3,
+                            self.lw / 10,
                             txt_color,
                             thickness=tf,
                             lineType=cv2.LINE_AA)
